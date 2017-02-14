d72797f58bc96f570a4975d0542e340d31e09617
talta <t.taltallc@gmail.com>
2017-02-13 20:40:37 -0500

    get request committed

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const {ShoppingList} = require('./models');
+const {Recipes} = require('./models');
 
 const jsonParser = bodyParser.json();
 const app = express();
@@ -15,13 +16,23 @@ app.use(morgan('common'));
 // we're going to add some items to ShoppingList
 // so there's some data to look at
 ShoppingList.create('beans', 2);
-ShoppingList.create('tomatoes', 3);
+ShoppingList.create('potatoes', 3);
 ShoppingList.create('peppers', 4);
+ShoppingList.create('onions', 1);
+
+Recipes.create(
+	'chocolate milk', ['cocoa', 'milk', 'sugar']);
+Recipes.create('sandwich', ['bread', 'knife', 'sesame seed butter']);
+Recipes.create('leftovers', ['bag-of-food-from-mom', 'microwave', 'plate', 'fork']);
 
 // when the root of this router is called with GET, return
 // all current ShoppingList items
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
+});
+
+app.get('/recipes', (req, res) => {
+	res.json(Recipes.get());
 });
 
 app.listen(process.env.PORT || 8080, () => {
